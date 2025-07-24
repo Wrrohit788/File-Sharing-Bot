1c7e8a96ab10e92a1e85bc758de7c32f84a6ebc0
Wrrohit788 <154300370+Wrrohit788@users.noreply.github.com>
2025-07-24 22:41:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ START_PIC = os.environ.get("START_PIC","")
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\n join @motumoviess")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "7807635630").split()):
+    for x in (os.environ.get("ADMINS", "").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
